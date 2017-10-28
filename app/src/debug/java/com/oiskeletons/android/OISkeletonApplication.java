b33ec778151e690f12477636516888113db2fadc
Rubin Apore <rubin.apore@gmail.com>
2017-10-28 10:49:11 +0000

    Use timber for logging

---
+++ app/src/debug/java/com/oiskeletons/android/OISkeletonApplication.java
@@ -0,0 +1,22 @@
+package com.oiskeletons.android.util;
+
+import android.app.Application;
+
+import timber.log.Timber;
+
+/**
+ * Created by rubin.apore on 10/28/17.
+ */
+
+public class OISkeletonApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Timber.plant(new Timber.DebugTree(){
+            @Override
+            protected String createStackElementTag(StackTraceElement element) {
+                return super.createStackElementTag(element) + ":" + element;
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/oiskeletons/android/util/ReleaseTimberLogTree.java
@@ -0,0 +1,10 @@
+package com.oiskeletons.android.util;
+
+import timber.log.Timber;
+
+/**
+ * Created by rubin.apore on 10/28/17.
+ */
+
+public class ReleaseTimberLogTree extends Timber {
+}
